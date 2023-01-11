4e92b45af6eb2143cfaf0371eda16887c7b4dc0b
Gabriel <68778251+GBRL-Web@users.noreply.github.com>
2023-01-11 15:42:11 +0100

    docs: Done working on project. :construction:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,17 +8,24 @@ import { AppComponent } from './app.component';
 import { HappybuttonComponent } from './components/happybutton/happybutton.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserService } from './user.service';
+import { DataCoursesComponent } from './components/data-courses/data-courses.component';
+import { FormCoursesComponent } from './components/form-courses/form-courses.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HappybuttonComponent,
-    UsersComponent
+    UsersComponent,
+    DataCoursesComponent,
+    FormCoursesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
